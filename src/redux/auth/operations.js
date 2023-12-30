a35dd0d20477d1317854e428358207bbd7bcabb4
Vladyslav Vagin <140642012+VladyslavVagin@users.noreply.github.com>
2023-12-30 16:58:41 +0100

    update

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -46,10 +46,8 @@ export const logIn = createAsyncThunk(
 export const logOut = createAsyncThunk('auth/logout', async (_, thunkAPI) => {
   try {
     await axios.post('/users/logout');
-    // After a successful logout, remove the token from the HTTP header
     clearAuthHeader();
   } catch (error) {
-    toast.error('Error, something went wrong!');
     return thunkAPI.rejectWithValue(error.message);
   }
 });
@@ -70,7 +68,6 @@ export const refreshUser = createAsyncThunk(
       const res = await axios.get('/users/current');
       return res.data;
     } catch (error) {
-      toast.error('Error, something went wrong!');
       return thunkAPI.rejectWithValue(error.message);
     }
   }
